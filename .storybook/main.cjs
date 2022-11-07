33bc3e7e7b55bf8b6915ed2ca8589d080d1d6324
leokbral <83135257+leokbral@users.noreply.github.com>
2022-11-07 14:40:17 -0300

    Update main.cjs

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -17,7 +17,7 @@ module.exports = {
   },
   viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
-      config.base = '/rockeseat/'
+      config.base = '/ds-lab/'
     }
 
 
